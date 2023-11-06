8b651b76fe974f31f5382e5a704e60b40545a79c
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2023-11-06 09:38:07 +0800

    Setup many to many

--- TShirtInventoryBackend/Models/Order.cs
+++ TShirtInventoryBackend/Models/Order.cs
@@ -3,10 +3,10 @@
     public class Order
     {
         public int Id { get; set; }
-        public Tshirt Tshirt { get; set; }
         public int Quantity { get; set; }
         public int UnitPrice { get; set; }
         public string Status { get; set; }
         public Customer Customer { get; set; }
+        public ICollection<Tshirt> Tshirts { get; set; }
     }
 }
