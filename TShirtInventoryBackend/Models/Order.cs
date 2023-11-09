3a378b0ae91f9e3a604e5537e3fe1e5a074583f8
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2023-11-09 17:43:35 +0800

    Migration Add OrderNumber Column

--- TShirtInventoryBackend/Models/Order.cs
+++ TShirtInventoryBackend/Models/Order.cs
@@ -3,6 +3,7 @@
     public class Order
     {
         public int Id { get; set; }
+        public int OrderNumber { get; set; }
         public string PaymentMethod { get; set; }
         public string Status { get; set; }
         public Customer Customer { get; set; }
