15510e12d54a4944a9776acf96c5baedd453a667
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2023-11-09 17:39:45 +0800

    Add Orders Controller

---
+++ TShirtInventoryBackend/Controllers/OrdersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TshirtInventoryBackend.Models.Request;
+using TshirtInventoryBackend.Repositories.Interface;
+
+namespace TshirtInventoryBackend.Controllers
+{
+    [Route("api/v1/customer")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrdersController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpPost("{id}/order")]
+        public async Task<IActionResult> CreateOrder(int id, OrderRequest orderRequest)
+        {
+            bool isCreated = await _unitOfWork.CreateOrder(id, orderRequest);
+            if (!isCreated)
+            {
+                return BadRequest();
+            }
+            return NoContent();
+        }
+    }
+}
--- TShirtInventoryBackend/Repositories/Interface/IUnitOfWork.cs
+++ TShirtInventoryBackend/Repositories/Interface/IUnitOfWork.cs
@@ -28,5 +28,6 @@ namespace TshirtInventoryBackend.Repositories.Interface
         Task<Tshirt> AddTshirt(TshirtRequest tshirt);
         Task UpdateTshirt(int id, TshirtRequest tshirtRequest);
         Task<Tshirt?> RemoveTshirt(int id);
+        Task<bool> CreateOrder(int customerId, OrderRequest orderRequest);
     }
 }
